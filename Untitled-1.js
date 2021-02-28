d17ddfbf133f3f39113c5cb110c5f8e07d252b53
Mikhail Pelagein <retvain@gmail.com>
2021-02-28 22:28:28 +0500

    js step 27 date full done!

--- Untitled-1.js
+++ Untitled-1.js
@@ -9,10 +9,11 @@ showdate.innerText = "Показать дату";
 hidedate.innerText = "Скрыть дату";
 
 document.body.appendChild(showdate);
+document.body.appendChild(hidedate);
 document.body.appendChild(time);
 
 
-
+//let timerID = setTimeout(showingdate, 1000);
 function showingdate ()
 {
     let date = new Date();
@@ -20,33 +21,14 @@ function showingdate ()
     let m = date.getMinutes();
     let s = date.getSeconds();
     time.innerHTML = h + " : " + m + " : " + s;
-    setTimeout(showingdate, 1000);
+    let timerID = setTimeout(showingdate, 1000);
+    hidedate.onclick = function () {
+        clearInterval(timerID);
+        timerID = null;
+        time.innerHTML = null;
+    }
 }
 
 showdate.onclick = showingdate;
 
-
-/*function showmydate ()
-{
-    let mydate = new Date();
-    let h = Date.getHours;
-    let m = Date.getMinutes;
-    let s = Date.getSeconds;
-
-    time.innerHTML = h + " : " + m + " : " + s;
-}
-
-function hidemaydate ()
-{
-    clearInterval(timerON);
-    clearTimeout(timerON);
-}*/
-
-/*let timerON = setInterval(showmydate, 1000);
-clearInterval(timerON);*/
-
-
-/*showdate.onclick = setInterval(showmydate, 1000);
-
-hidedate.onclick = hidemaydate;*/
 
